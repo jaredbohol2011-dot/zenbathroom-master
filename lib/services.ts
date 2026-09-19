0a1a581521deebb4d0353061d365b1ec5a3ac520
jaredbohol2011-dot <jared.bohol2011@gmail.com>
2026-09-19 19:18:24 +0100

    Add files via upload

---
+++ lib/services.ts
@@ -0,0 +1,12 @@
+// src/lib/services.ts
+export const services = [
+  { name: "Tiling", path: "/tiling" },
+  { name: "Plumbing", path: "/plumbing" },
+  { name: "Electrical Works", path: "/electrical-works" },
+  {
+    name: "Modern Floor and wall Finishes",
+    path: "/modern-floor-and-wall-finishes",
+  },
+  { name: "Bathroom cabinet design", path: "/bathroom-cabinet-design" },
+  { name: "Free Quote and Consultation", path: "/free-consultation-and-quote" },
+];
